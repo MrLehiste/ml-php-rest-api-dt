eea70b1ac6ee57b20fd9d0f909f84aba74d93853
MrLehiste <hr.lehiste@gmail.com>
2016-03-13 14:05:10 -0700

    api post

--- web/index.php
+++ web/index.php
@@ -1,25 +1,19 @@
 <?php
+use 
+	Symfony\Component\HttpFoundation\Request,
+	Symfony\Component\HttpFoundation\Response;
 require('../vendor/autoload.php');
 $app = new Silex\Application();
 $app['debug'] = true;
 // Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
+$app->register(new Silex\Provider\MonologServiceProvider(), array('monolog.logfile' => 'php://stderr', ));
 // Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
+$app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/views', ));
 
 // Loading environment variables (for dev env)
 $dotenv = new Dotenv\Dotenv(__DIR__);
-try{
-  $dotenv->load();
-  $app['monolog']->addDebug('$dotenv->load(); finished');
-}
-catch(Exception $e) {
-  $app['monolog']->addDebug('$dotenv->load(); FAILED');
-}
+try{ $dotenv->load(); $app['monolog']->addDebug('$dotenv->load(); finished'); }
+catch(Exception $e) { $app['monolog']->addDebug('$dotenv->load(); FAILED'); }
 
 // Our web handlers
 $app->get('/', function() use($app) {
@@ -27,14 +21,7 @@ $app->get('/', function() use($app) {
   return $app['twig']->render('index.twig');
 });
 
-$dbopts = parse_url(getenv('DATABASE_URL')); 
-$app->register(new Herrera\Pdo\PdoServiceProvider(),
-    array(
-        'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
-        'pdo.username' => $dbopts["user"],
-        'pdo.password' => $dbopts["pass"]
-    )
-);
+$dbopts = parse_url(getenv('DATABASE_URL'));
 $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
     'db.options' => array(
         'driver'   => 'pdo_pgsql',
@@ -46,30 +33,34 @@ $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
     ),
 ));
 
-$app->get('/db/', function() use($app) {
-  $app['monolog']->addDebug( getenv('DATABASE_URL') );
-  return json_encode(parse_url(getenv('DATABASE_URL')));
+//$app->get('/db/', function() use($app) { $app['monolog']->addDebug( getenv('DATABASE_URL') ); return json_encode(parse_url(getenv('DATABASE_URL'))); });
+
+$app->get('/users/', function (Request $request) use ($app) {
+    //$names = $app['db']->fetchAll('SELECT first_name as name FROM users');
+    //return $app['twig']->render('database.twig', array('names' => $names));
+    $users = $app['db']->fetchAll('SELECT * FROM users');
+    return $app->json($users, 201);
 });
-
-$app->get('/users', function () use ($app) {
-    $names = array();
-    $names = $app['db']->fetchAll('SELECT first_name as name FROM users');
-
-    return $app['twig']->render('database.twig', array('names' => $names));
+$app->get('/users/{id}', function (Request $request, $id) use ($app) {
+    $sql = "SELECT * FROM users WHERE user_id = ?";
+    $post = $app['db']->fetchAssoc($sql, array((int) $id));
+    return $app->json($post, 201);
 });
-$app->get('/x/users/', function() use($app) {
-  $st = $app['pdo']->prepare('SELECT first_name as name FROM users');
-  $st->execute();
-
-  $names = array();
-  while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
-    $app['monolog']->addDebug('Row ' . $row['first_name']);
-    $names[] = $row;
-  }
-
-  return $app['twig']->render('database.twig', array(
-    'names' => $names
-  ));
+$app->post('/users/', function (Request $request) use ($app) {
+    $user = array(
+    	'first_name' => $request->get('first_name'),
+    	'last_name' => $request->get('last_name'),
+        'email' => $request->get('email'),
+        'password' => $request->get('password'),
+        'is_active' => $request->get('is_active'),
+    );
+    $app['db']->insert('users', $user);
+    return new Response("User " . $app['db']->lastInsertId() . " created", 201);
+});
+$app->delete('/users/{id}', function (Request $request, $id) use ($app) {
+    $sql = "DELETE FROM users WHERE id = ?";
+    $app['db']->executeUpdate($sql, array((int) $id));
+    return new Response("User " . $id . " deleted", 303);
 });
 
 $app->run();
